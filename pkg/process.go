224b62674335d252a67b6ac7d07bf4761f3b7232
emperorsixpacks <andrewdvd10@gmail.com>
2025-05-29 18:21:46 +0100

    feat: gracefully shutdown process

--- pkg/process.go
+++ pkg/process.go
@@ -3,9 +3,15 @@ package pkg
 import (
 	"fmt"
 	"io"
+	"os"
 	"syscall"
+	"time"
 )
 
+func isRunning(pid int) bool {
+	err := syscall.Kill(pid, 0)
+	return err != nil
+}
 func StartProcess(command string, logPath io.Writer, args ...string) error {
 
 	cmd := execCommand(command, args...)
@@ -20,5 +26,24 @@ func StartProcess(command string, logPath io.Writer, args ...string) error {
 		return err
 	}
 	fmt.Println(cmd.Process.Pid)
+	return nil
+}
+
+func StopProcess(pid int) error {
+	p, err := os.FindProcess(pid)
+	if err != nil {
+		return err
+	}
+	err = p.Signal(syscall.SIGTERM)
+	if err != nil {
+		return err
+	}
+	time.Sleep(time.Second * 3)
+	if running := isRunning(pid); running {
+		err = p.Signal(syscall.SIGTERM)
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
