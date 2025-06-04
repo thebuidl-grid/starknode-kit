a0fdf25b9e80986beaba58dcc2bde933bb2bcfb0
emperorsixpacks <andrewdvd10@gmail.com>
2025-06-04 15:40:31 +0100

    feat: added config types

--- pkg/types/config_types.go
+++ pkg/types/config_types.go
@@ -1,1 +1,12 @@
 package types
+
+type StarkNodeKitConfig struct {
+	ExecutionCientSettings ClientConfig `yaml:"execution_client"`
+	ConsensusCientSettings ClientConfig `yaml:"consensus_client"`
+}
+
+type ClientConfig struct {
+	Name    string   `yaml:"name"`
+	Network string   `yaml:"network"`
+	Port    []string `yaml:"ports"`
+}
