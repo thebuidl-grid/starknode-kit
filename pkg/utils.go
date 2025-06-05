fab2b6c5febab0e0c1dfadef20a859983ed7e8ea
emperorsixpacks <andrewdvd10@gmail.com>
2025-06-05 16:42:17 +0100

    feat: update config file

--- pkg/utils.go
+++ pkg/utils.go
@@ -9,15 +9,13 @@ import (
 )
 
 var (
-	config     StarkNodeKitConfig
-	_          = loadConfig()
 	InstallDir = path.Join(getHomeDir(), "starcknode-kit")
 
 	InstallClientsDir = path.Join(InstallDir, "ethereum_clients")
 
 	jwtDir         = path.Join(InstallDir, "ethereum_clients", "jwt")
 	JWTPath        = path.Join(jwtDir, "jwt.hex")
-	configDir     = path.Join(InstallDir, "config")
+	configDir      = path.Join(InstallDir, "config")
 	yamlConfigPath = fmt.Sprintf("%s/stacknode.yaml", configDir)
 )
 
@@ -52,18 +50,38 @@ func getHomeDir() string {
 	return homeDir
 }
 
-func loadConfig() error {
-	_, err := os.ReadFile(yamlConfigPath)
+func LoadConfig() (StarkNodeKitConfig, error) {
+	var cfg StarkNodeKitConfig
+	cfgByt, err := os.ReadFile(yamlConfigPath)
 	if err != nil {
-		config = defaultConfig()
+		return StarkNodeKitConfig{}, err
+	}
+	err = yaml.Unmarshal(cfgByt, cfg)
+	if err != nil {
+		return StarkNodeKitConfig{}, err
+	}
+	return cfg, nil
+}
+
+func UpdateStackNodeConfig(config StarkNodeKitConfig) error {
+	if err := os.MkdirAll(configDir, 0755); err != nil {
+		return fmt.Errorf("failed to update config file: %w", err)
+	}
+	cfg, err := yaml.Marshal(config)
+	if err != nil {
+		return err
+	}
+	err = os.WriteFile(yamlConfigPath, cfg, 0600)
+	if err != nil {
+		return err
 	}
 	return nil
 }
 
 func CreateStackNodeConfig() error {
 	default_config := defaultConfig()
-  if err := os.MkdirAll(configDir, 0755); err != nil {
-		return fmt.Errorf("failed to create JWT directory: %w", err)
+	if err := os.MkdirAll(configDir, 0755); err != nil {
+		return fmt.Errorf("failed to create config file: %w", err)
 	}
 	cfg, err := yaml.Marshal(default_config)
 	if err != nil {
