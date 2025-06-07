0beb2fa5aaac7568e868ff69c514f23d864f1bf1
emperorsixpacks <andrewdvd10@gmail.com>
2025-06-07 16:36:11 +0100

    ref: run el and cl

--- cli/cmd/commands/runCommand.go
+++ cli/cmd/commands/runCommand.go
@@ -1,6 +1,12 @@
 package commands
 
-import "github.com/spf13/cobra"
+import (
+	"buidlguidl-go/pkg"
+	"buidlguidl-go/pkg/clients"
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
 
 var RunCommand = &cobra.Command{
 	Use:   "run",
@@ -11,4 +17,56 @@ defined settings and manages them as part of your node stack.`,
 	Run: runcommand,
 }
 
-func runcommand(cmd *cobra.Command, args []string) {}
+func runcommand(cmd *cobra.Command, args []string) {
+	config, err := pkg.LoadConfig()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	el := config.ExecutionCientSettings
+	cl := config.ConsensusCientSettings
+	elClient, err := pkg.GetExecutionClient(string(el.Name))
+	if err != nil {
+		fmt.Println("Supported execution clients are:")
+		fmt.Println(" - geth")
+		fmt.Println(" - reth")
+		return
+	}
+	clClient, err := pkg.GetConsensusClient(string(cl.Name))
+	if err != nil {
+		fmt.Println("Supported consensus clients are:")
+		fmt.Println(" - lighhouse")
+		fmt.Println(" - prysm")
+		return
+	}
+
+	err = pkg.IsInstalled(elClient)
+	if err != nil {
+		fmt.Printf("Client \"%s\" is not installed.\n", elClient)
+		fmt.Printf("Please run: starknode add -e %s\n", elClient)
+		return
+	}
+	err = pkg.IsInstalled(clClient)
+
+	if err != nil {
+		fmt.Printf("Client \"%s\" is not installed.\n", clClient)
+		fmt.Printf("Please run: starknode add -c %s\n", clClient)
+		return
+	}
+	switch clClient {
+	case pkg.ClientLighthouse:
+		if err = clients.StartLightHouse(cl.Port...); err != nil {
+			fmt.Println(err)
+			return
+		}
+	case pkg.ClientPrysm:
+		if err = clients.StartPrsym(cl.Port...); err != nil {
+			fmt.Println(err)
+			return
+		}
+	default:
+		fmt.Printf("Client \"%s\" is not installed.\n", clClient)
+		fmt.Printf("Please run: starknode add -c %s\n", clClient)
+		return
+	}
+}
