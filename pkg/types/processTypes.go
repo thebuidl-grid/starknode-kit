63359135deea705fe69ef34c4de6dd06fa0e08f2
emperorsixpacks <andrewdvd10@gmail.com>
2025-06-09 17:01:25 +0100

    ref: moved process info to types pkg

--- pkg/types/processTypes.go
+++ pkg/types/processTypes.go
@@ -1,11 +1,23 @@
 package types
 
-type Process struct {
-	Processes []process `yaml:"processes"`
+import "time"
+
+type ProcessInfo struct {
+	PID      int           `json:"pid"`
+	Name     string        `json:"name"`
+	Status   string        `json:"status"`
+	Uptime   time.Duration `json:"uptime"`
+	CPUUsage float64       `json:"cpu_usage"`
+	MemUsage uint64        `json:"mem_usage"`
 }
 
-type process struct {
-	Name ClientType `yaml:"name"`
-	Pid  int        `yaml:"pid"`
-	Path string     `yaml:"path"`
+// EthereumMetrics holds blockchain metrics
+type EthereumMetrics struct {
+	CurrentBlock uint64  `json:"current_block"`
+	HighestBlock uint64  `json:"highest_block"`
+	SyncPercent  float64 `json:"sync_percent"`
+	PeerCount    int     `json:"peer_count"`
+	IsSyncing    bool    `json:"is_syncing"`
+	GasPrice     string  `json:"gas_price"`
+	NetworkName  string  `json:"network"`
 }
