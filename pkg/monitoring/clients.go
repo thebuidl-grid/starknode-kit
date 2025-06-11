54ce56aa4ac09b00e62916c0003be4d16ed89d34
emperorsixpacks <andrewdvd10@gmail.com>
2025-06-11 15:23:11 +0100

    ref: monitoring

--- pkg/monitoring/clients.go
+++ pkg/monitoring/clients.go
@@ -2,196 +2,28 @@ package monitoring
 
 import (
 	// "context"
+
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
 	"path/filepath"
+	"starknode-kit/pkg"
+	t "starknode-kit/pkg/types"
+	"starknode-kit/pkg/utils"
 
 	// "regexp"
-	"strconv"
+
 	"strings"
 	"time"
-
-	"starknode-kit/pkg"
-	"starknode-kit/pkg/types"
 )
 
-// getGethSyncStatus gets sync status from Geth's HTTP API
-func GetGethSyncStatus() types.SyncInfo {
-	syncInfo := types.SyncInfo{IsSyncing: false, SyncPercent: 100.0}
-
-	// Try to get sync status from Geth's HTTP API
-	client := &http.Client{Timeout: 2 * time.Second}
-
-	// Geth eth_syncing call
-	payload := `{"jsonrpc":"2.0","method":"eth_syncing","params":[],"id":1}`
-	resp, err := client.Post("http://localhost:8545", "application/json", strings.NewReader(payload))
-	if err != nil {
-		return syncInfo
-	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return syncInfo
-	}
-
-	var result map[string]interface{}
-	if err := json.Unmarshal(body, &result); err != nil {
-		return syncInfo
-	}
-
-	if syncResult, ok := result["result"]; ok {
-		if syncResult == false {
-			// Not syncing = fully synced
-			syncInfo.IsSyncing = false
-			syncInfo.SyncPercent = 100.0
-		} else if syncData, ok := syncResult.(map[string]interface{}); ok {
-			// Currently syncing
-			syncInfo.IsSyncing = true
-
-			if currentHex, ok := syncData["currentBlock"].(string); ok {
-				if current, err := parseHexInt(currentHex); err == nil {
-					syncInfo.CurrentBlock = current
-				}
-			}
-
-			if highestHex, ok := syncData["highestBlock"].(string); ok {
-				if highest, err := parseHexInt(highestHex); err == nil {
-					syncInfo.HighestBlock = highest
-					if highest > 0 {
-						syncInfo.SyncPercent = float64(syncInfo.CurrentBlock) / float64(highest) * 100
-					}
-				}
-			}
-		}
-	}
-
-	// Get peer count
-	peerPayload := `{"jsonrpc":"2.0","method":"net_peerCount","params":[],"id":2}`
-	peerResp, err := client.Post("http://localhost:8545", "application/json", strings.NewReader(peerPayload))
-	if err == nil {
-		defer peerResp.Body.Close()
-		peerBody, _ := io.ReadAll(peerResp.Body)
-		var peerResult map[string]interface{}
-		if json.Unmarshal(peerBody, &peerResult) == nil {
-			if peerHex, ok := peerResult["result"].(string); ok {
-				if peers, err := parseHexInt(peerHex); err == nil {
-					syncInfo.PeersCount = int(peers)
-				}
-			}
-		}
-	}
-
-	return syncInfo
-}
-
-// getRethSyncStatus gets sync status from Reth's HTTP API
-func GetRethSyncStatus() types.SyncInfo {
-	// Similar to Geth but Reth might have different endpoints
-	return getGethSyncStatus() // For now, use same logic
-}
-
-// getLighthouseSyncStatus gets sync status from Lighthouse's HTTP API
-func GetLighthouseSyncStatus() types.SyncInfo {
-	syncInfo := types.SyncInfo{IsSyncing: false, SyncPercent: 100.0}
-
-	client := &http.Client{Timeout: 2 * time.Second}
-
-	// Lighthouse HTTP API
-	resp, err := client.Get("http://localhost:5052/eth/v1/node/syncing")
-	if err != nil {
-		return syncInfo
-	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return syncInfo
-	}
-
-	var result map[string]interface{}
-	if err := json.Unmarshal(body, &result); err != nil {
-		return syncInfo
-	}
-
-	if data, ok := result["data"].(map[string]interface{}); ok {
-		if isSyncing, ok := data["is_syncing"].(bool); ok {
-			syncInfo.IsSyncing = isSyncing
-		}
-
-		if headSlot, ok := data["head_slot"].(string); ok {
-			if head, err := strconv.ParseUint(headSlot, 10, 64); err == nil {
-				syncInfo.CurrentBlock = head
-			}
-		}
-
-		if syncDistance, ok := data["sync_distance"].(string); ok {
-			if distance, err := strconv.ParseUint(syncDistance, 10, 64); err == nil {
-				syncInfo.HighestBlock = syncInfo.CurrentBlock + distance
-				if syncInfo.HighestBlock > 0 {
-					syncInfo.SyncPercent = float64(syncInfo.CurrentBlock) / float64(syncInfo.HighestBlock) * 100
-				}
-			}
-		}
-	}
-
-	return syncInfo
-}
-
-// getPrysmSyncStatus gets sync status from Prysm's HTTP API
-func GetPrysmSyncStatus() types.SyncInfo {
-	syncInfo := types.SyncInfo{IsSyncing: false, SyncPercent: 100.0}
-
-	client := &http.Client{Timeout: 2 * time.Second}
-
-	// Prysm HTTP API
-	resp, err := client.Get("http://localhost:5052/eth/v1/node/syncing")
-	if err != nil {
-		return syncInfo
-	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return syncInfo
-	}
-
-	var result map[string]interface{}
-	if err := json.Unmarshal(body, &result); err != nil {
-		return syncInfo
-	}
-
-	// Similar to Lighthouse
-	if data, ok := result["data"].(map[string]interface{}); ok {
-		if isSyncing, ok := data["is_syncing"].(bool); ok {
-			syncInfo.IsSyncing = isSyncing
-		}
-
-		if headSlot, ok := data["head_slot"].(string); ok {
-			if head, err := strconv.ParseUint(headSlot, 10, 64); err == nil {
-				syncInfo.CurrentBlock = head
-			}
-		}
-
-		if syncDistance, ok := data["sync_distance"].(string); ok {
-			if distance, err := strconv.ParseUint(syncDistance, 10, 64); err == nil {
-				syncInfo.HighestBlock = syncInfo.CurrentBlock + distance
-				if syncInfo.HighestBlock > 0 {
-					syncInfo.SyncPercent = float64(syncInfo.CurrentBlock) / float64(syncInfo.HighestBlock) * 100
-				}
-			}
-		}
-	}
-
-	return syncInfo
-}
+// utils.ParseHexInt parses a hex string to uint64
 
 // GetEthereumMetrics gets blockchain metrics
-func GetEthereumMetrics() types.EthereumMetrics {
-	metrics := types.EthereumMetrics{
+func GetEthereumMetrics() t.EthereumMetrics {
+	metrics := t.EthereumMetrics{
 		NetworkName: "Mainnet",
 		IsSyncing:   false,
 		SyncPercent: 100.0,
@@ -205,10 +37,10 @@ func GetEthereumMetrics() types.EthereumMetrics {
 	if err == nil {
 		defer resp.Body.Close()
 		body, _ := io.ReadAll(resp.Body)
-		var result map[string]interface{}
+		var result map[string]any
 		if json.Unmarshal(body, &result) == nil {
 			if blockHex, ok := result["result"].(string); ok {
-				if block, err := parseHexInt(blockHex); err == nil {
+				if block, err := utils.ParseHexInt(blockHex); err == nil {
 					metrics.CurrentBlock = block
 				}
 			}
@@ -221,10 +53,10 @@ func GetEthereumMetrics() types.EthereumMetrics {
 	if err == nil {
 		defer gasResp.Body.Close()
 		gasBody, _ := io.ReadAll(gasResp.Body)
-		var gasResult map[string]interface{}
+		var gasResult map[string]any
 		if json.Unmarshal(gasBody, &gasResult) == nil {
 			if gasPriceHex, ok := gasResult["result"].(string); ok {
-				if gasPrice, err := parseHexInt(gasPriceHex); err == nil {
+				if gasPrice, err := utils.ParseHexInt(gasPriceHex); err == nil {
 					// Convert wei to gwei
 					gweiPrice := float64(gasPrice) / 1e9
 					metrics.GasPrice = fmt.Sprintf("%.1f gwei", gweiPrice)
@@ -239,10 +71,10 @@ func GetEthereumMetrics() types.EthereumMetrics {
 	if err == nil {
 		defer peerResp.Body.Close()
 		peerBody, _ := io.ReadAll(peerResp.Body)
-		var peerResult map[string]interface{}
+		var peerResult map[string]any
 		if json.Unmarshal(peerBody, &peerResult) == nil {
 			if peerHex, ok := peerResult["result"].(string); ok {
-				if peers, err := parseHexInt(peerHex); err == nil {
+				if peers, err := utils.ParseHexInt(peerHex); err == nil {
 					metrics.PeerCount = int(peers)
 				}
 			}
@@ -300,13 +132,4 @@ func GetLatestLogs(clientName string, lines int) []string {
 	}
 
 	return result
-}
-
-// parseHexInt parses a hex string to uint64
-func parseHexInt(hexStr string) (uint64, error) {
-	// Remove 0x prefix if present
-	if strings.HasPrefix(hexStr, "0x") {
-		hexStr = hexStr[2:]
-	}
-	return strconv.ParseUint(hexStr, 16, 64)
 }
