80e17e2852ffc9099fc8fc692d0ff8a362412e9e
emperorsixpacks <andrewdvd10@gmail.com>
2025-07-05 18:28:00 +0100

    ref: added starknet install dir

--- pkg/constants.go
+++ pkg/constants.go
@@ -10,6 +10,7 @@ var (
 	InstallDir = path.Join(getHomeDir(), "starknode-kit")
 
 	InstallClientsDir = path.Join(InstallDir, "ethereum_clients")
+	InstallStarknetDir = path.Join(InstallDir, "starknet_clients")
 
 	JwtDir      = path.Join(InstallDir, "ethereum_clients", "jwt")
 	JWTPath     = path.Join(JwtDir, "jwt.hex")
@@ -17,6 +18,12 @@ var (
 	JunoDataDir = path.Join(InstallDir, "juno-data")
 	ConfigPath  = fmt.Sprintf("%s/starknode.yaml", ConfigDir)
 	EnvFIlePath = fmt.Sprintf("%s/.starknode.env", ConfigDir)
+<<<<<<< Updated upstream
+||||||| Stash base
+	Banner      = figure.NewColorFigure("Starknode kit", "slant", "green", true)
+=======
+	Banner      = figure.NewColorFigure("Starknode kit", "small", "green", true)
+>>>>>>> Stashed changes
 )
 
 func getHomeDir() string {
