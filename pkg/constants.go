251076b9bbd4c1fcbb3b136314331e89c948639d
emperorsixpacks <andrewdvd10@gmail.com>
2025-07-05 18:36:47 +0100

    ref constants

--- pkg/constants.go
+++ pkg/constants.go
@@ -4,12 +4,14 @@ import (
 	"fmt"
 	"os"
 	"path"
+
+	"github.com/common-nighthawk/go-figure"
 )
 
 var (
 	InstallDir = path.Join(getHomeDir(), "starknode-kit")
 
-	InstallClientsDir = path.Join(InstallDir, "ethereum_clients")
+	InstallClientsDir  = path.Join(InstallDir, "ethereum_clients")
 	InstallStarknetDir = path.Join(InstallDir, "starknet_clients")
 
 	JwtDir      = path.Join(InstallDir, "ethereum_clients", "jwt")
@@ -18,12 +20,7 @@ var (
 	JunoDataDir = path.Join(InstallDir, "juno-data")
 	ConfigPath  = fmt.Sprintf("%s/starknode.yaml", ConfigDir)
 	EnvFIlePath = fmt.Sprintf("%s/.starknode.env", ConfigDir)
-<<<<<<< Updated upstream
-||||||| Stash base
 	Banner      = figure.NewColorFigure("Starknode kit", "slant", "green", true)
-=======
-	Banner      = figure.NewColorFigure("Starknode kit", "small", "green", true)
->>>>>>> Stashed changes
 )
 
 func getHomeDir() string {
