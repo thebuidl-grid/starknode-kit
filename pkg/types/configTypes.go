9ab0eec85d1e2cccc822aed65e7c0da30ae3d3f3
emperorsixpacks <andrewdvd10@gmail.com>
2025-07-16 14:57:34 +0100

    feat: added juno config to types

--- pkg/types/configTypes.go
+++ pkg/types/configTypes.go
@@ -20,12 +20,20 @@ type StarkNodeKitConfig struct {
 	Network                string       `yaml:"network"`
 	ExecutionCientSettings ClientConfig `yaml:"execution_client"`
 	ConsensusCientSettings ClientConfig `yaml:"consensus_client"`
+	JunoConfig             JunoConfig   `yaml:"juno_client"`
 }
 
 type ClientConfig struct {
 	Name                ClientType `yaml:"name"`
-	Network             string     `yaml:"network,omitempty"`
 	ExecutionType       string     `yaml:"execution_type,omitempty"`
 	Port                []int      `yaml:"ports"`
 	ConsensusCheckpoint string     `yaml:"consensus_checkpoint,omitempty"`
+}
+
+type JunoConfig struct {
+	Port        string   `yaml:"port"`
+	UseSnapshot bool     `yaml:"use_snapshot"`
+	DataDir     string   `yaml:"data_dir"`
+	EthNode     string   `yaml:"eth_node"`
+	Environment []string `yaml:"environment"`
 }
