1daec653304cd0a2ac42413aeb347266399e95fa
emperorsixpacks <andrewdvd10@gmail.com>
2025-07-16 15:25:51 +0100

    ref: removed junodata from constants

--- pkg/constants.go
+++ pkg/constants.go
@@ -17,7 +17,6 @@ var (
 	JwtDir      = path.Join(InstallDir, "ethereum_clients", "jwt")
 	JWTPath     = path.Join(JwtDir, "jwt.hex")
 	ConfigDir   = path.Join(InstallDir, "config")
-	JunoDataDir = path.Join(InstallDir, "juno-data")
 	ConfigPath  = fmt.Sprintf("%s/starknode.yaml", ConfigDir)
 	EnvFIlePath = fmt.Sprintf("%s/.starknode.env", ConfigDir)
 	Banner      = figure.NewColorFigure("Starknode kit", "slant", "green", true)
