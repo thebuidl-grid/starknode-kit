06174183a0e9200881da98fd0cf25c5b0f710332
emperorsixpacks <andrewdvd10@gmail.com>
2025-08-08 14:34:36 +0100

    feat: added validator config

--- pkg/types/configTypes.go
+++ pkg/types/configTypes.go
@@ -31,25 +31,42 @@ type IClient interface {
 	Start() error
 }
 
-type StarkNodeKitConfig struct {
-	// WalletAddress          string       `yaml:"wallet_address"`
-	// PrivateKey             string       `yaml:"private_key"`
-	Network                string       `yaml:"network"`
-	ExecutionCientSettings ClientConfig `yaml:"execution_client"`
-	ConsensusCientSettings ClientConfig `yaml:"consensus_client"`
-	JunoConfig             JunoConfig   `yaml:"juno_client"`
-}
+type (
+	StarkNodeKitConfig struct {
+		Network                string       `yaml:"network"`
+		IsValidatorNode        bool         `yaml:"is_validator_node"`
+		Wallet                 WalletConfig `wallet:"wallet"`
+		ExecutionCientSettings ClientConfig `yaml:"execution_client"`
+		ConsensusCientSettings ClientConfig `yaml:"consensus_client"`
+		JunoConfig             JunoConfig   `yaml:"juno_client"`
+	}
 
-type ClientConfig struct {
-	Name                ClientType `yaml:"name"`
-	ExecutionType       string     `yaml:"execution_type,omitempty"`
-	Port                []int      `yaml:"ports"`
-	ConsensusCheckpoint string     `yaml:"consensus_checkpoint,omitempty"`
-}
+	ClientConfig struct {
+		ExecutionType       string     `yaml:"execution_type,omitempty"`
+		Port                []int      `yaml:"ports"`
+		ConsensusCheckpoint string     `yaml:"consensus_checkpoint,omitempty"`
+		Name                ClientType `yaml:"name"`
+	}
 
-type JunoConfig struct {
-	Port            int      `yaml:"port"`
-	EthNode         string   `yaml:"eth_node"`
-	IsValidatorNode bool     `yaml:"is_validator_node"`
-	Environment     []string `yaml:"environment"`
-}
+	JunoConfig struct {
+		Port        int      `yaml:"port"`
+		EthNode     string   `yaml:"eth_node"`
+		Environment []string `yaml:"environment"`
+	}
+
+	WalletConfig struct {
+		WalletAddress string `yaml:"wallet_address"`
+		PrivateKey    string `yaml:"private_key"`
+	}
+
+	ValidatorConfig struct {
+		ProviderConfig struct {
+			JunoRPC string `json:"http" yaml:"juno_rpc_http"`
+			JunoWS  string `json:"ws" yaml:"juno_rpc_ws"`
+		} `json:"provider" yaml:"provider_config"`
+		SignerConfig struct {
+			OperationalAddress string `json:"operational_address"`
+			WalletPrivateKey   string `json:"privateKey"`
+		} `json:"signer" yaml:"signer"`
+	}
+)
