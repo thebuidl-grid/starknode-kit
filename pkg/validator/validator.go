43d11a039544e4c7602166a25747d53350ece0de
emperorsixpacks <andrewdvd10@gmail.com>
2025-09-17 16:17:17 +0100

    feat: validator esitmate fees

--- pkg/validator/validator.go
+++ pkg/validator/validator.go
@@ -2,8 +2,12 @@ package validator
 
 import (
 	"context"
+	"fmt"
+	"math/big"
+	"strconv"
 
 	"github.com/NethermindEth/juno/core/felt"
+	"github.com/NethermindEth/starknet.go/account"
 	"github.com/NethermindEth/starknet.go/rpc"
 	starkutils "github.com/NethermindEth/starknet.go/utils"
 	"github.com/thebuidl-grid/starknode-kit/pkg/constants"
@@ -12,9 +16,10 @@ import (
 )
 
 func GetValidatorInfo(rpcProvider *rpc.Provider, wallet types.Wallet) (types.ValidatorInfo, error) {
-	accnt, err := newAccount(wallet, rpcProvider)
+	address, err := starkutils.HexToFelt(wallet.Address)
 	if err != nil {
 		return types.ValidatorInfo{}, err
+
 	}
 
 	contractAddress, err := starkutils.HexToFelt(constants.StakingContract)
@@ -25,7 +30,7 @@ func GetValidatorInfo(rpcProvider *rpc.Provider, wallet types.Wallet) (types.Val
 	txn := rpc.FunctionCall{
 		ContractAddress:    contractAddress,
 		EntryPointSelector: starkutils.GetSelectorFromNameFelt("get_staker_info_v1"),
-		Calldata:           []*felt.Felt{accnt.Address},
+		Calldata:           []*felt.Felt{address},
 	}
 
 	result, err := rpcProvider.Call(context.Background(), txn, rpc.BlockID{Tag: "latest"})
@@ -51,4 +56,95 @@ func GetValidatorBalance(rpcProvider *rpc.Provider, wallet types.Wallet) (float6
 	}
 	balanceStark := starkutils.FRIToSTRK(balance)
 	return balanceStark, nil
+}
+
+// StakeStark performs the staking process for a given wallet configuration and network.
+// It checks for sufficient balance, builds the necessary transactions (approve, stake, set_commission),
+// sends them as a multi-call, and waits for the transaction to be confirmed.
+func StakeStark(network string, rpcProvider *rpc.Provider, wallet types.WalletConfig) error {
+	accnt, err := newAccount(wallet.Wallet, rpcProvider)
+	if err != nil {
+		return fmt.Errorf("failed to create account: %w", err)
+	}
+
+	// Check for sufficient balance before proceeding
+	balance, err := utils.CheckBalance(rpcProvider, accnt.Address)
+	if err != nil {
+		return fmt.Errorf("failed to check balance: %w", err)
+	}
+
+	err = approveStakes(network, accnt, rpcProvider, balance)
+	if err != nil {
+		return err
+	}
+
+	// Build transactions
+	stackingAddr, err := starkutils.HexToFelt(constants.StakingContract)
+	if err != nil {
+		return fmt.Errorf("failed to convert staking contract address to felt: %w", err)
+	}
+
+	rewardAddress, err := starkutils.HexToFelt(wallet.RewardAddress)
+	if err != nil {
+		return fmt.Errorf("failed to convert reward address to felt: %w", err)
+	}
+
+	stakeTxn := rpc.FunctionCall{
+		ContractAddress:    stackingAddr,
+		EntryPointSelector: starkutils.GetSelectorFromNameFelt("stake"),
+		Calldata:           []*felt.Felt{rewardAddress, accnt.Address, constants.Stakes[network][0]},
+	}
+
+	commissionInt, err := strconv.Atoi(wallet.StakeCommision)
+	if err != nil {
+		return fmt.Errorf("failed to convert stake commission to integer: %w", err)
+	}
+
+	// Commission is in basis points (e.g., 10% = 1000)
+	commissionFelt := starkutils.BigIntToFelt(
+		big.NewInt(int64(uint16(commissionInt * 100))),
+	)
+
+	setCommissionTxn := rpc.FunctionCall{
+		ContractAddress:    stackingAddr,
+		EntryPointSelector: starkutils.GetSelectorFromNameFelt("set_commission"),
+		Calldata:           []*felt.Felt{commissionFelt},
+	}
+
+	invokeTxn, err := estimateFee(accnt, []rpc.FunctionCall{stakeTxn, setCommissionTxn}, balance)
+	if err != nil {
+		return err
+	}
+
+	return executeTxn(accnt, invokeTxn)
+}
+
+func approveStakes(network string, accnt *account.Account, rpcProvider *rpc.Provider, balance *felt.Felt) error {
+	stakingAddr, err := starkutils.HexToFelt(constants.StakingContract)
+	if err != nil {
+		return err
+	}
+	starkAddr, err := starkutils.HexToFelt(constants.StrkTokenAddress)
+	if err != nil {
+		return err
+	}
+	allowance, err := getAllowance(rpcProvider, accnt.Address, stakingAddr)
+	if err != nil {
+		return err
+	}
+	allowanceFelt := starkutils.FeltToBigInt(allowance)
+	if allowanceFelt.Cmp(starkutils.FeltToBigInt(constants.Stakes[network][0])) >= 0 {
+		return nil
+	}
+	approveTxn := rpc.FunctionCall{
+		ContractAddress:    starkAddr,
+		EntryPointSelector: starkutils.GetSelectorFromNameFelt("approve"),
+		Calldata:           []*felt.Felt{stakingAddr, constants.Stakes[network][0], constants.Stakes[network][1]},
+	}
+	invokeTxn, err := estimateFee(accnt, []rpc.FunctionCall{approveTxn}, balance)
+	if err != nil {
+		return err
+	}
+
+	return executeTxn(accnt, invokeTxn)
 }
--- pkg/validator/validator_.go
+++ pkg/validator/validator_.go
@@ -4,10 +4,13 @@ import (
 	"context"
 	"fmt"
 	"math/big"
+	"time"
 
+	"github.com/NethermindEth/juno/core/felt"
 	"github.com/NethermindEth/starknet.go/account"
 	"github.com/NethermindEth/starknet.go/rpc"
 	starknet "github.com/NethermindEth/starknet.go/utils"
+	"github.com/thebuidl-grid/starknode-kit/pkg/constants"
 	"github.com/thebuidl-grid/starknode-kit/pkg/types"
 	"github.com/thebuidl-grid/starknode-kit/pkg/utils"
 )
@@ -38,13 +41,77 @@ func newAccount(wallet types.Wallet, rpcProvider *rpc.Provider) (*account.Accoun
 	return userAccount, nil
 }
 
-func execute(ctx context.Context, user_account *account.Account, txn []rpc.InvokeFunctionCall) (rpc.AddInvokeTransactionResponse, error) {
-
-	resp, err := user_account.BuildAndSendInvokeTxn(ctx, txn, nil)
+func getAllowance(rpcProvider *rpc.Provider, owner *felt.Felt, spender *felt.Felt) (*felt.Felt, error) {
+	contractAddress, err := starknet.HexToFelt(constants.StrkTokenAddress)
 	if err != nil {
-		return rpc.AddInvokeTransactionResponse{}, err
+		return nil, err
 	}
-	transactionUrl := fmt.Sprintf("https://sepolia.voyager.online/tx/%s", utils.FormatTransactionHash(resp.Hash))
-	fmt.Println("Transaction successfull, view here: ", transactionUrl)
-	return resp, nil
+
+	txn := rpc.FunctionCall{
+		ContractAddress:    contractAddress,
+		EntryPointSelector: starknet.GetSelectorFromNameFelt("allowance"),
+		Calldata:           []*felt.Felt{owner, spender},
+	}
+
+	result, err := rpcProvider.Call(context.Background(), txn, rpc.BlockID{Tag: "latest"})
+	if err != nil {
+		return nil, err
+	}
+	return result[0], nil
+}
+
+func estimateFee(accnt *account.Account, calls []rpc.FunctionCall, balance *felt.Felt) (*rpc.BroadcastInvokeTxnV3, error) {
+	fmt.Println("Estimating transaction fees...")
+	invokeTxn, feesEstimate, err := utils.EstimateGasFee(accnt, calls)
+	if err != nil {
+		return nil, fmt.Errorf("failed to estimate gas fee: %w", err)
+	}
+	estimatedFee := feesEstimate[0].OverallFee
+	estimatedFeeStark := starknet.FRIToSTRK(estimatedFee)
+	fmt.Printf("Estimated fee: %.6f STRK. Adjusting for a higher success rate...\n", estimatedFeeStark)
+
+	if balance.Cmp(estimatedFee) < 0 {
+		needed := starknet.FRIToSTRK(estimatedFee)
+		have := starknet.FRIToSTRK(balance)
+		return nil, fmt.Errorf("insufficient balance for transaction. Have: %.6f STRK, Need: %.6f STRK", have, needed)
+	}
+
+	invokeTxn.ResourceBounds = starknet.FeeEstToResBoundsMap(feesEstimate[0], 1.5)
+
+	// Re-sign the transaction as resource bounds have changed
+	err = accnt.SignInvokeTransaction(context.Background(), invokeTxn)
+	if err != nil {
+		return nil, fmt.Errorf("failed to sign transaction: %w", err)
+	}
+
+	return invokeTxn, nil
+}
+
+func executeTxn(accnt *account.Account, invokeTxn *rpc.BroadcastInvokeTxnV3) error {
+	err = accnt.SignInvokeTransaction(context.Background(), invokeTxn)
+	if err != nil {
+		return fmt.Errorf("failed to sign transaction: %w", err)
+	}
+
+	// Send the transaction
+	fmt.Println("Sending transactions...")
+	resp, err := accnt.SendTransaction(context.Background(), invokeTxn)
+	if err != nil {
+		return fmt.Errorf("failed to send transaction: %w", err)
+	}
+
+	fmt.Printf("Transaction successfully submitted! Transaction hash: %s\n", utils.FormatTransactionHash(resp.Hash))
+	fmt.Println("⏳ Waiting for transaction confirmation...")
+
+	// Wait for receipt
+	receipt, err := accnt.WaitForTransactionReceipt(context.Background(), resp.Hash, 15*time.Second)
+	if err != nil {
+		transactionUrl := fmt.Sprintf("https://sepolia.voyager.online/tx/%s", utils.FormatTransactionHash(resp.Hash))
+		fmt.Printf("Transaction failed or timed out. View details here: %s\n", transactionUrl)
+		return fmt.Errorf("error waiting for transaction receipt: %w", err)
+	}
+
+	transactionUrl := fmt.Sprintf("https://sepolia.voyager.online/tx/%s", utils.FormatTransactionHash(receipt.Hash))
+	fmt.Printf("Transaction successful! View details here: %s\n", transactionUrl)
+	return nil
 }
